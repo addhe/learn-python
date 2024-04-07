b6afd31322065c526da29cf2972b0a58586cc4ff
Addhe Warman Putra <addhe.warman@gmail.com>
2024-04-08 06:39:26 +0700

    a function that accept a string and calculate the number of uppercase and lowercase letters - updated

--- functions-and-methods/calculate-number-uppercase-lowercase-letters.py
+++ functions-and-methods/calculate-number-uppercase-lowercase-letters.py
@@ -3,19 +3,18 @@ letters and lower case letters"""
 
 
 def up_low(s):
-    lowercase = 0
-    uppercase = 0
+    dict = {"upper": 0, "lower": 0}
 
     for char in s:
         if char.isupper():
-            uppercase += 1
+            dict["upper"] += 1
         elif char.islower():
-            lowercase += 1
+            dict["lower"] += 1
         else:
             pass
     print(f"Original String: {s}")
-    print(f"Lowercase Count: {lowercase}")
-    print(f"Uppercase Count: {uppercase}")
+    print(f'Lowercase Count: {dict["upper"]}')
+    print(f'Uppercase Count: {dict["lower"]}')
 
 
 s = "Hai Mr. Awan, how are you today? would you like a Coffee?"
