96dcde832158c2df4e98738ea3bfb70ead43d6ca
Addhe Warman Putra <addhe.warman@gmail.com>
2025-04-25 18:59:08 +0700

    reset my python learning repo

--- functions-and-methods/check-number-inclusive-high-or-low.py
+++
@@ -1,9 +0,0 @@
-# Write a function that checks whether a number is in given range ( inclusive of high and low )
-def ran_check(num, low, high):
-    if num in range(low, high + 1):
-        print(f"{num} is in range of low and high")
-    else:
-        print("not in range of low and high")
-
-
-ran_check(10, 4, 14)
